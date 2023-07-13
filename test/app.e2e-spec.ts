55595ed80048e9e33fa527d57d5bb66b347d6990
PatrickSilvaMenezes <patricksilvamenezes@gmail.com>
2023-07-13 11:14:35 -0300

    test: filter colaborators e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { BadRequestException, HttpStatus, INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
-import { randomUUID } from 'crypto';
-import { CreateCompanyDto } from 'src/company/presentation/models';
+import { INestApplication } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import * as request from 'supertest';
+import { randomUUID } from 'crypto';
+import { AppModule } from './../src/app.module';
+import { CreateCompanyDto } from '../src/company/presentation';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -21,7 +21,7 @@ describe('AppController (e2e)', () => {
 
   
   describe('POST /company/create', () => {
-    test('should create a new company if valid params was provided', async () => {
+    test('should return 200 and create a new company if valid params was provided', async () => {
       const dto: CreateCompanyDto = {
         name: `${randomUUID() + "valid_company"}`,
         nickname: "valid_nickname_company",
@@ -72,33 +72,63 @@ describe('AppController (e2e)', () => {
       expect(response.status).toEqual(400)
     })
     
-      test('should return 403 if company already exists', async () => {
-        const dto: CreateCompanyDto = {
-          name: "valid_company",
-          nickname: "valid_nickname_company",
-          registration: "valid_registration",
-          active: true
+    test('should return 403 if company already exists', async () => {
+      const dto: CreateCompanyDto = {
+        name: "valid_company",
+        nickname: "valid_nickname_company",
+        registration: "valid_registration",
+        active: true
+      }
+      await request(app.getHttpServer()).post('/company/create').send(dto)
+      const response = await request(app.getHttpServer()).post('/company/create').send(dto)
+      expect(response.status).toEqual(403)
+    })
+  })
+  
+  describe('POST /company/collaborators/filter', () => {
+    test('should return 200 and filter an valid colaborator with valid params provided', async () => {
+      await prisma.enterprise.update({
+        where: {
+          name: "valid_company"
+        },
+        data: {
+          colaborators: {
+            create: {
+              name: "valid_colaborator_name",
+              email: "valid_colaborator@email.com"
+            }
+          }
         }
-        await request(app.getHttpServer()).post('/company/create').send(dto)
-        const response = await request(app.getHttpServer()).post('/company/create').send(dto)
-        expect(response.status).toEqual(403)
       })
+
+      const response = await request(app.getHttpServer()).post('/company/collaborators/filter').send({
+        companyName: "valid_company",
+        filter: "valid_colaborator_name"
+      })
+      expect(response.status).toEqual(200)
+    })
+    test('should return 404 if no colaborator was found', async () => { 
+      const response = await request(app.getHttpServer()).post('/company/collaborators/filter').send({
+        companyName: "invalid_company",
+        filter: "invalid_colaborator_name"
+      })
+      expect(response.status).toEqual(404)
+    })
   })
 
-    describe('POST /company/read', () => {
-      test('should return all companies registered', async () => {
-        const response = await request(app.getHttpServer()).post('/company/read')
-        console.log(response)
-        expect(response.status).toEqual(200)
-      })
-      test('should return 404 if something went wrong while trying to read companies', async () => { 
-        await prisma.$transaction([
-          prisma.enterprise.deleteMany()
-        ])
-        const response = await request(app.getHttpServer()).post('/company/read')
-        expect(response.status).toEqual(404)
-      })
-    });
+  describe('POST /company/read', () => {
+    test('should return 200 and read all companies registered', async () => {
+      const response = await request(app.getHttpServer()).post('/company/read')
+      expect(response.status).toEqual(200)
+    })
+    test('should return 404 if something went wrong while trying to read companies', async () => {
+      await prisma.$transaction([
+        prisma.enterprise.deleteMany()
+      ])
+      const response = await request(app.getHttpServer()).post('/company/read')
+      expect(response.status).toEqual(404)
+    })
+  });
     
 })
 
